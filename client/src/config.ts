0664bd34167af24c596d1a7f14058cc53f721562
DieserMarkus <29387829+DieserMarkus@users.noreply.github.com>
2021-12-17 13:25:15 +0100

    fix: changed callbackUrl

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com
 export const authConfig = {
   domain: 'stuff.eu.auth0.com',
   clientId: 'lOUb1fCX51rIB9sEQqzOOoyhru1RH8fW',
-  callbackUrl: 'http://localhost:3000/callback'
+  //callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://fetch.m4rkus.com/callback'
 }
